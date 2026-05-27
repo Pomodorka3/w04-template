049363619ac5a770a63debbbb09d775005e71b5a
Michael Berezovsky <pomodorka3@gmail.com>
2026-05-27 19:54:08 +0200

    chore: change terraform cfg

--- ops/cloud/iac/main.tf
+++ ops/cloud/iac/main.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.5.0"
 
+   backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "stcanteentfstate"
+    container_name       = "tfstate"
+    key                  = "canteen-dev.terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
